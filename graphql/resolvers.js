59fee7209ec77e36e2c70688f1469cb7ed67e3f1
eunajjing <high.euna@gmail.com>
2019-02-14 18:48:44 +0900

    이중 스키마 정의

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,7 +1,16 @@
+
+const exPerson = {
+    name: "name value",
+    age: 25,
+    gender: "intersex"
+}
+
+
 const resolvers = {
     Query: {
-        name: () => "name value"
+        person: () => exPerson
     }
 }
+
 
 export default resolvers;
